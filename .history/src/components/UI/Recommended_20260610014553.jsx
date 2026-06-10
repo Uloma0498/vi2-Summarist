ef35c564e4f092281e1d8cec5ac90409504eaa93
Home <ejuloma07@gmail.com>
2026-06-10 02:49:52 -0400

    premium pill

---
+++ .history/src/components/UI/Recommended_20260610014553.jsx
@@ -0,0 +1,44 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import Book from "./Book";
+
+
+const Recommended = () => {
+    const [recommended, setRecommended] = useState(null)
+
+    useEffect(() => {
+        const fetchRecommended = async () => {
+            try{
+                const { data } = await axios.get(`https://us-central1-summaristt.cloudfunctions.net/getBooks?status=recommended`)
+                setRecommended(data);
+            } catch (error) {
+                console.error("Error fetching book:", error);
+            }
+        }
+        fetchRecommended();
+    }, []);
+
+    if(!recommended) return <div>Loading.</div>;
+
+    return (
+     <>
+     <div className="for-you__title">
+        Recommended For You
+     </div>
+     <div className="for-you__sub--title">
+        We think you'll like these
+     </div>
+     <div className="for-you__recommended--books">
+      {recommended.map((book) => (
+        <div className="book" key={book.id}>
+        <Book book={book} />
+        {book.subscriptionRequired && }
+        </div>
+      ))}
+    </div>
+     </>
+    )
+}
+
+export default Recommended;
